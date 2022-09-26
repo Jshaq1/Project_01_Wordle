323b091606ca041ba979c58a64ca4c41bd4ec529
Jordan <Jordanshaqallen@gmail.com>
2022-09-26 18:52:49 +0800

    Key input working thus far

--- script.js
+++ script.js
@@ -1,35 +1,70 @@
 //MODEL
 //Define Global Variables
+let answer = getAnswer()
+let guess = []
+let nextSpace = 1;
 
-const words = []
-const answer = getAnswer()
-const guess1 = []
 
-const keyContainer = document.getElementById('keyboard')
 
 //Generating Random Answer
 function getAnswer(){
 return validWords[Math.floor(Math.random()*validWords.length)];
 }
 
-//UpdateView
-//Function to select word
 
+// KEYBOARD EVENT LISTENER //
+const keyContainer = document.getElementById('keyboard')
 const keys = document.getElementsByClassName("key");
 for (let key of keys) {
-  key.addEventListener("click", renderInput);
+  key.addEventListener("click", keyInput);
 }
 
-function renderInput(event){
-    const inputKey = document.getElementsByClassName('key')
-    const inputDiv = document.getElementsByClassName('character')
+// HANDLE KEY PRESS //
+function keyInput(event){
     const clickedElement = event.target
-    const inputResult = clickedElement.dataset.key
-    guess1.push(inputResult)
+    const inputLetter = clickedElement.dataset.key
+    
+// HANDLE PRESSING ENTER TO SUBMIT GUESS //
+//Need to do this first so that when they press enter there is a response before they can add more letters to the next row down
+if(inputLetter == 'enter'){
+  return enterInput()
+}
+// HITTING A LETTER/KEY //
+else{
+  updateGuess(inputLetter)
+}}
 
-    console.log(guess1)
+function enterInput(){
+  if(guess < 5){
+    console.log('Your answer is too short!')
+  }
+}
+
+
+//Place input letter into 'GUESS' array, and renders view in HTML.
+function updateGuess (inputLetter) {
+  const currentGuess = guess
+  
+  if (currentGuess && currentGuess.length < 6 ){
+    currentGuess.push(inputLetter);
+  }
+
+
+
+
+
+  //Using a variable to change the ID referenced by adding 1 to it each keypress.
+  //Id of each input div in HTML set 1-30 
+  const nextSpaceDiv  = document.getElementById(nextSpace)
+  nextSpace = nextSpace +1
+  nextSpaceDiv.textContent = inputLetter
+
 }
 
 //Logging stuff
 getAnswer()
 console.log(answer)
+
+
+
+
