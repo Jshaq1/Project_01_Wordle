6513dc51e7a446884629f73d53fef26fb6a3662e
Jordan <Jordanshaqallen@gmail.com>
2022-09-27 19:10:45 +0800

    Updated color change

--- script.js
+++ script.js
@@ -3,12 +3,14 @@
 let answer = getAnswer()
 let guess = []
 let nextSpace = 1;
+let row = 0;
+
 
 
 
 //Generating Random Answer
-function getAnswer(){
-return validWords[Math.floor(Math.random()*validWords.length)]
+function getAnswer() {
+  return validWords[Math.floor(Math.random() * validWords.length)]
 
 }
 
@@ -23,74 +25,74 @@ for (let key of keys) {
 // ------------------------------------------------- //
 
 // HANDLE KEY PRESS //
-function keyInput(event){
-    const clickedElement = event.target
-    const inputLetter = clickedElement.dataset.key
-    
-// HANDLE PRESSING ENTER TO SUBMIT GUESS //
-//Need to do this first so that when they press enter there is a response before they can add more letters to the next row down
-if(inputLetter == 'enter'){
-  return enterInput()
+function keyInput(event) {
+  const clickedElement = event.target
+  const inputLetter = clickedElement.dataset.key
+
+  // HANDLE PRESSING ENTER TO SUBMIT GUESS //
+  //Need to do this first so that when they press enter there is a response before they can add more letters to the next row down
+  if (inputLetter == 'enter') {
+    return enterInput()
+  }
+  // HITTING A LETTER/KEY //
+  if (guess.length < 5) {
+    updateGuess(inputLetter)
+
+  }
 }
-// HITTING A LETTER/KEY //
-if(guess.length <5){
-  updateGuess(inputLetter)
-  
-}}
 
 //---------------------------------------------------//
 
-function enterInput(){
+function enterInput() {
+
   console.log(guess)
-  if(guess.length < 5){
+  if (guess.length < 5) {
     return alert('your answer is too short')
   }
-  if(guess.length == 5){
-    const tile = document.getElementById('tile')
-    guess.forEach((inputLetter, index) => {
-    if(answer.includes(inputLetter)){
-      let tile = document.getElementById(index);
-      tile
-
-
-    }
+  if (guess.length == 5) {
     
-  })
-  guess = guess.join('')
-  checkWinner()
+    getResult()
+    guess = guess.join('')
+    checkWinner()
 
-}}
+  }row = row +1;
+}
 
+//---------------------------------------------------//
 
-function checkWinner (){
-  
-
-  if (guess == answer){
+function checkWinner() {
+  if (guess == answer) {
     guess = []
-
     return alert('you win idiot')
 
-  }if(guess !== answer){
+  } if (guess !== answer) {
     guess = []
     return alert('try again idiot')
   }
 }
 
 //Place input letter into 'GUESS' array, and renders view in HTML.
-function updateGuess (inputLetter) {
+function updateGuess(inputLetter) {
   const currentGuess = guess
-  
-  if (currentGuess.length < 5){
+  if (currentGuess.length < 5) {
     currentGuess.push(inputLetter);
   }
 
-  //Using a variable to change the ID referenced by adding 1 to it each keypress.
-  //Id of each input div in HTML set 1-30 
-  const nextSpaceDiv  = document.getElementById(nextSpace)
-  nextSpace = nextSpace +1
+  const nextSpaceDiv = document.getElementById(nextSpace)
+  nextSpace = nextSpace + 1
   nextSpaceDiv.textContent = inputLetter
-  
+
 }
+
+function getResult() {
+  guess.forEach((inputLetter, index) => {
+    const id = row * 5 + (index + 1)
+    document.getElementById(id).style.backgroundColor = 'purple'
+    if (answer.includes(inputLetter)) {
+   }
+  })
+}
+
 
 
 
